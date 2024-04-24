2022bee74642c7dd0c89266065c58abfddd2ba85
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-24 10:23:57 +0900

    Add `MintKit` to dependencies

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ let package = Package(
     name: "mint-update",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/yonaskolb/Mint.git", from: "0.17.5")
     ],
     targets: [
         .executableTarget(
             name: "mint-update",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "MintKit", package: "Mint")
             ]
         ),
     ]
