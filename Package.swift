102e41ca6597f2228ff8baeeadc12e246057a4a6
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-24 12:00:25 +0900

    add `mint-update` to products

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "mint-update",
+    products: [
+        .executable(name: "mint-update", targets: ["mint-update"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/yonaskolb/Mint.git", from: "0.17.5")
